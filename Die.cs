8d202c386ea247927f7af7302234b7454b1c497f
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-16 09:04:34 -0400

    Class Done

--- Die.cs
+++ Die.cs
@@ -20,7 +20,20 @@ namespace Topic_5._5___More_Classes__Electric_Boogaloo_
         public Die(int roll)
         {
             _generator = new Random();
-            _roll = roll;
+
+            if (roll < 1)
+            {
+                roll = 1;
+            }
+            else if (roll > 6)
+            {
+                roll = 6;
+            }
+            else
+            {
+                _roll = roll;
+            }
+
         }
 
         public int Roll { get { return _roll; } }
@@ -37,7 +50,54 @@ namespace Topic_5._5___More_Classes__Electric_Boogaloo_
 
         public void DrawRoll()
         {
-
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
         }
 
 
